96e0c04aadd0e2a02af5f05e2fba1b8ae1147ca0
Paul <paulerogerson@gmail.com>
2019-09-25 09:07:26 +0100

    implement disco ball method

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ class SupremeDisco {
   constructor() {
     this.music = true;
     this.danceFloor = 'sticky';
+    this.discoBallSpinning = false;
   }
   dance() {
     console.log('boogy booogie boooogzz!');
+  }
+  spinDiscoBall() {
+    this.discoBallSpinning = true;
   }
 }
